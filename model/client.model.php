ff80fa651e44896af641248e8a8c226599330786
aitagueye <gueyeanita2626@gmail.com>
2026-07-02 02:28:47 +0000

    Feat 2: Initialisation du tableau de donnees clients

---
+++ model/client.model.php
@@ -0,0 +1,6 @@
+<?php
+
+$clients = [
+    0 => ['nomPrenom' => 'Aita GUEYE', 'tel' => '770001233', 'address' => 'Mbour'],
+    1 => ['nomPrenom' => 'Awa LY', 'tel' => '771001233', 'address' => 'grand Dakar'],
+];

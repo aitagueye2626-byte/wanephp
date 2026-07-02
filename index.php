a8b5647b4c53ce64583e6b7929fec6ced6751ac2
aitagueye <gueyeanita2626@gmail.com>
2026-07-02 03:08:22 +0000

    Feat 2: changement du menu index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-
 require_once __DIR__ . "/utils/error.php";
 require_once __DIR__ . "/utils/validator.php";
+require_once __DIR__ . "/utils/view.utils.php";
 
 require_once __DIR__ . "/model/product.model.php";
 require_once __DIR__ . "/model/client.model.php";
@@ -16,32 +16,36 @@ do {
     echo "\n====================================\n";
     echo "          MENU PRINCIPAL            \n";
     echo "====================================\n";
+    echo "--- INCREMENT 1 (PRODUITS) ---\n";
     echo "1. Enregistrer un produit\n";
-    echo "2. Lister les produits non archivés\n";
-    echo "3. Archiver un produit\n";
+    echo "2. Archiver un produit\n";
+    echo "3. Lister les produits non archivés\n";
+    echo "\n--- INCREMENT 2 (CLIENTS) ---\n";
     echo "4. Enregistrer un client\n";
-    echo "5. Lister tous les clients\n";
-    echo "0. Quitter l'application\n";
+    echo "5. Lister les clients qui n'ont pas de commande\n";
+    echo "\n0. Quitter l'application\n";
     echo "====================================\n";
     
     $choix = saisie("Votre choix : ");
 
     switch (trim($choix)) {
         case "1":
-            saveProduct(); 
+            ajouterProduitController(); 
             break;
         case "2":
-            listerProduits($products); 
+            archiverProduitController(); 
             break;
         case "3":
-            archiverProduit();
+            listerProduits($products); 
             break;
+
         case "4":
             ajouterClientController(); 
             break;
         case "5":
-            listerClients($clients); 
+            listerClientsSansCommandeController(); 
             break;
+
         case "0":
             echo "\nFermeture de l'application. Au revoir !\n";
             break;
